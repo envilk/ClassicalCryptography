9f1a499418f0a0e0364b2bc21e6d07bf587cd1ea
envilk <envilkc@gmail.com>
2019-09-22 20:46:22 +0200

    Fixed proxy

--- src/no/hvl/dat159/config/ProxyConfig.java
+++ src/no/hvl/dat159/config/ProxyConfig.java
@@ -11,6 +11,6 @@ public class ProxyConfig {
 	
 	// set the proxy server address and port where it listens to incoming connections
 	public static final String SERVER = "localhost";
-	public static final int PORT = 9082;
+	public static final int PORT = 9085;
 
 }
